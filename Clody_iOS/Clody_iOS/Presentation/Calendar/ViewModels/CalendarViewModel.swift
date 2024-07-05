17cf1018fa209cee8869544abdba615d14502e21
kim-seonwoo <sw204011@gmail.com>
2024-07-05 11:50:09 +0900

    [Feat/#7] 캘린더 뷰 모델 추가

--- Clody_iOS/Clody_iOS/Presentation/Calendar/ViewModels/CalendarViewModel.swift
+++ Clody_iOS/Clody_iOS/Presentation/Calendar/ViewModels/CalendarViewModel.swift
@@ -5,4 +5,27 @@
 //  Created by Seonwoo Kim on 6/28/24.
 //
 
-import Foundation
+import UIKit
+
+import RxSwift
+
+protocol CalendarViewModelType {
+    associatedtype Input
+    associatedtype Output
+
+    func transform(from input: Input, disposeBag: RxSwift.DisposeBag) -> Output
+}
+
+final class CalendarViewModel: CalendarViewModelType {
+
+    struct Input {
+    }
+
+    struct Output {
+    }
+    
+    func transform(from input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+        return output
+    }
+}
