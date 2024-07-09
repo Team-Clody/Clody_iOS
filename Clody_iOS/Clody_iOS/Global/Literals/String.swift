06e5219357eaff4741831a735e811502b094b5e3
Nya128 <nanalovesbap@gmail.com>
2024-07-10 00:34:37 +0900

    [Feat/#9] 커스텀 Alert 컴포넌트 UI 구현

--- Clody_iOS/Clody_iOS/Global/Literals/String.swift
+++ Clody_iOS/Clody_iOS/Global/Literals/String.swift
@@ -19,4 +19,20 @@ enum I18N {
         static let emailError = "이메일 형식을 확인해주세요"
         static let charLimit = "/ 10"
     }
+    
+    enum Alert {
+        static let logoutTitle = "로그아웃 하시겠어요?"
+        static let logoutMessage = "기다릴게요, 다음에 다시 만나요!"
+        static let withdrawTitle = "서비스를 탈퇴하시겠어요?"
+        static let withdrawMessage = "작성하신 일기와 받은 답장 및 클로버가 모두 삭제되며 복구할 수 없어요."
+        static let saveDiaryTitle = "일기를 저장할까요?"
+        static let saveDiaryMessage = "저장한 일기는 수정이 어려워요."
+        static let deleteDiaryTitle = "정말 일기를 삭제할까요?"
+        static let deleteDiaryMessage = "삭제한 일기는 복원이 어려워요."
+        static let cancel = "취소"
+        static let logout = "로그아웃"
+        static let withdraw = "탈퇴할래요"
+        static let save = "저장하기"
+        static let delete = "삭제하기"
+    }
 }
---
+++ Clody_iOS/Clody_iOS/Presentation/Common/Component/ClodyAlert.swift
@@ -0,0 +1,138 @@
+//
+//  ClodyAlert.swift
+//  Clody_iOS
+//
+//  Created by 김나연 on 7/9/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+enum AlertType {
+    case logout
+    case withdraw
+    case deleteDiary
+    case saveDiary
+}
+
+class ClodyAlert: UIView {
+    
+    // MARK: - UI Components
+    
+    private let titleLabel = UILabel()
+    private let messageLabel = UILabel()
+    let leftButton = UIButton()
+    let rightButton = UIButton()
+    
+    // MARK: - Properties
+    
+    var type: AlertType
+    
+    // MARK: - Life Cycles
+
+    init(
+        type: AlertType,
+        title: String,
+        message: String,
+        rightButtonText: String
+    ) {
+        self.type = type
+        super.init(frame: .zero)
+        
+        setStyle(with: title, message, rightButtonText)
+        setHierarchy()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Extensions
+
+extension ClodyAlert {
+    
+    // MARK: - Methods
+    
+    private func setStyle(
+        with title: String,
+        _ message: String,
+        _ rightButtonText: String
+    ) {
+        backgroundColor = .white
+        makeCornerRound(radius: 12)
+        
+        switch type {
+        case .logout, .saveDiary:
+            rightButton.backgroundColor = .mainYellow
+            rightButton.setTitleColor(.grey01, for: .normal)
+        case .withdraw, .deleteDiary:
+            rightButton.backgroundColor = .redCustom
+            rightButton.setTitleColor(.white, for: .normal)
+        }
+        
+        titleLabel.do {
+            $0.text = title
+            $0.textColor = .grey01
+            $0.font = .pretendard(.body1_semibold)
+            $0.textAlignment = .center
+        }
+        
+        messageLabel.do {
+            $0.text = message
+            $0.textColor = .grey04
+//            $0.font = .pretendard(.popup_regular)
+            $0.textAlignment = .center
+        }
+        
+        leftButton.do {
+            $0.backgroundColor = .grey07
+            $0.setTitle(I18N.Alert.cancel, for: .normal)
+            $0.titleLabel?.font = .pretendard(.body3_semibold)
+            $0.setTitleColor(.grey04, for: .normal)
+            $0.makeCornerRound(radius: 8)
+        }
+        
+        rightButton.do {
+            $0.setTitle(rightButtonText, for: .normal)
+            $0.titleLabel?.font = .pretendard(.body3_semibold)
+            $0.makeCornerRound(radius: 8)
+        }
+    }
+    
+    private func setHierarchy() {
+        self.addSubviews(titleLabel, messageLabel, leftButton, rightButton)
+    }
+    
+    private func setLayout() {
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(27)
+            $0.horizontalEdges.greaterThanOrEqualToSuperview().inset(20)
+            $0.centerX.equalToSuperview()
+        }
+        
+        messageLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(9)
+            $0.horizontalEdges.greaterThanOrEqualToSuperview().inset(20)
+            $0.centerX.equalToSuperview()
+        }
+        
+        leftButton.snp.makeConstraints {
+            $0.width.equalTo(rightButton)
+            $0.height.equalTo(42)
+            $0.top.equalTo(messageLabel.snp.bottom).offset(34)
+            $0.leading.equalToSuperview().inset(20)
+            $0.bottom.equalToSuperview().inset(20)
+        }
+        
+        rightButton.snp.makeConstraints {
+            $0.height.equalTo(leftButton)
+            $0.leading.equalTo(leftButton.snp.trailing).offset(9)
+            $0.trailing.equalToSuperview().inset(20)
+            $0.centerY.equalTo(leftButton)
+        }
+    }
+}
