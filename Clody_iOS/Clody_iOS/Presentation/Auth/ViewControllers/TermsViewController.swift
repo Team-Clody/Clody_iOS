a436315a5be93d66bf32955df8168bfb0f0fa2bb
Nya128 <nanalovesbap@gmail.com>
2024-07-15 20:51:57 +0900

    [Feat/#26] 약관동의 화면 RxSwift input, output 연결

--- Clody_iOS/Clody_iOS/Presentation/Auth/ViewControllers/TermsViewController.swift
+++ Clody_iOS/Clody_iOS/Presentation/Auth/ViewControllers/TermsViewController.swift
@@ -15,6 +15,7 @@ final class TermsViewController: UIViewController {
     
     // MARK: - Properties
     
+    private let viewModel = TermsViewModel()
     private let disposeBag = DisposeBag()
     
     // MARK: - UI Components
@@ -42,6 +43,72 @@ final class TermsViewController: UIViewController {
 private extension TermsViewController {
 
     func bindViewModel() {
+        let allAgreeTextButtonTapEvent = rootView.allAgreeTextButton.rx.tap
+            .map {
+                let isSelected = self.rootView.allAgreeIconButton.isSelected
+                return !isSelected
+            }
+            .asSignal(onErrorJustReturn: false)
+        
+        let allAgreeIconButtonTapEvent = rootView.allAgreeIconButton.rx.tap
+            .map {
+                let isSelected = self.rootView.allAgreeIconButton.isSelected
+                return !isSelected
+            }
+            .asSignal(onErrorJustReturn: false)
+        
+        let input = TermsViewModel.Input(
+            allAgreeTextButtonTapEvent: allAgreeTextButtonTapEvent,
+            allAgreeIconButtonTapEvent: allAgreeIconButtonTapEvent,
+            nextButtonTapEvent: rootView.nextButton.rx.tap.asSignal()
+        )
+        let output = viewModel.transform(from: input, disposeBag: disposeBag)
+        
+        _ = rootView.agreeTermsIconButton.rx.tap
+            .map {
+                let isSelected = self.rootView.agreeTermsIconButton.isSelected
+                self.rootView.agreeTermsIconButton.isSelected = !isSelected
+                return !isSelected
+            }
+            .bind(to: input.agreeTermsButtonTapEvent)
+            .disposed(by: disposeBag)
+        
+        _ = rootView.agreePrivacyIconButton.rx.tap
+            .map {
+                let isSelected = self.rootView.agreePrivacyIconButton.isSelected
+                self.rootView.agreePrivacyIconButton.isSelected = !isSelected
+                return !isSelected
+            }
+            .bind(to: input.agreePrivacyButtonTapEvent)
+            .disposed(by: disposeBag)
+        
+        output.twoAgreeCombineEvent
+            .drive(onNext: { isBothAgree in
+                self.rootView.allAgreeIconButton.isSelected = isBothAgree
+            })
+            .disposed(by: disposeBag)
+        
+        output.allAgreeButtonMergeEvent
+            .drive(onNext: { isAllAgree in
+                input.agreeTermsButtonTapEvent.accept(isAllAgree)
+                input.agreePrivacyButtonTapEvent.accept(isAllAgree)
+                self.rootView.allAgreeIconButton.isSelected = isAllAgree
+                self.rootView.agreeTermsIconButton.isSelected = isAllAgree
+                self.rootView.agreePrivacyIconButton.isSelected = isAllAgree
+            })
+            .disposed(by: disposeBag)
+
+        output.nextButtonIsEnabled
+            .drive(onNext: { isEnabled in
+                self.rootView.nextButton.setEnabledState(to: isEnabled)
+            })
+            .disposed(by: disposeBag)
+        
+        output.pushViewController
+            .drive(onNext: { _ in
+                self.navigationController?.pushViewController(NicknameViewController(), animated: true)
+            })
+            .disposed(by: disposeBag)
     }
 
     func setUI() {
