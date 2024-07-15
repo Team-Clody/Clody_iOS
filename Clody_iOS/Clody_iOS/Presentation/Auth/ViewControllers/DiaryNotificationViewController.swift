a6c59c24b50f1c9240ba54d140f041be22826f36
Nya128 <nanalovesbap@gmail.com>
2024-07-16 04:13:58 +0900

    [Feat/#26] 알림 설정 화면 RxSwift 바인딩(바텀 시트 제외)

--- Clody_iOS/Clody_iOS/Presentation/Auth/ViewControllers/DiaryNotificationViewController.swift
+++ Clody_iOS/Clody_iOS/Presentation/Auth/ViewControllers/DiaryNotificationViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 import RxCocoa
+import RxGesture
 import RxSwift
 import Then
 
@@ -15,6 +16,7 @@ final class DiaryNotificationViewController: UIViewController {
     
     // MARK: - Properties
     
+    private let viewModel = DiaryNotificationViewModel()
     private let disposeBag = DisposeBag()
     
     // MARK: - UI Components
@@ -42,6 +44,29 @@ final class DiaryNotificationViewController: UIViewController {
 private extension DiaryNotificationViewController {
 
     func bindViewModel() {
+        let timeSettingViewTapEvent: Signal<Void> = rootView.timeSettingView.rx.tapGesture()
+            .when(.recognized)
+            .asSignal(onErrorJustReturn: .init())
+            .map { _ in }
+        
+        let input = DiaryNotificationViewModel.Input(
+            timeSettingViewTapEvent: timeSettingViewTapEvent,
+            completeButtonTapEvent: rootView.completeButton.rx.tap.asSignal(),
+            setNextButtonTapEvent: rootView.setNextButton.rx.tap.asSignal()
+        )
+        let output = viewModel.transform(from: input, disposeBag: disposeBag)
+        
+        output.showBottomSheet
+            .drive(onNext: {
+                // TODO: 알림 시간 설정 바텀시트 띄우기
+            })
+            .disposed(by: disposeBag)
+        
+        output.pushViewController
+            .drive(onNext: {
+                self.navigationController?.pushViewController(OnBoardingViewController(), animated: true)
+            })
+            .disposed(by: disposeBag)
     }
 
     func setUI() {
