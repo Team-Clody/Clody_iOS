c8ec59c7410c49cee080f1e114b8e3f05e1e8862
kim-seonwoo <sw204011@gmail.com>
2024-07-17 20:12:40 +0900

    [Feat/#39] 일기 작성 화면 삭제 구현

--- Clody_iOS/Clody_iOS/Presentation/WritingDiary/ViewControllers/WritingDiaryViewController.swift
+++ Clody_iOS/Clody_iOS/Presentation/WritingDiary/ViewControllers/WritingDiaryViewController.swift
@@ -59,7 +59,11 @@ private extension WritingDiaryViewController {
             tapSaveButton: rootView.saveButton.rx.tap.asSignal(),
             tapAddButton: rootView.addButton.rx.tap.asSignal(),
             tapBackButton: rootView.navigationBarView.backButton.rx.tap.asSignal(),
-            kebabButtonTap: kebabButtonTap
+            updateKebobRelay: kebabButtonTap,
+            tapDeleteButton: deleteBottomSheetView.deleteContainer.rx.tapGesture()
+                .when(.recognized)
+                .map { _ in }
+                .asSignal(onErrorJustReturn: ())
         )
         
         let output = viewModel.transform(from: input, disposeBag: disposeBag)
@@ -200,7 +204,7 @@ private extension WritingDiaryViewController {
             .when(.recognized)
             .subscribe(onNext: { [weak self] _ in
                 self?.dismissBottomSheet(animated: true, completion: {
-                    self?.showClodyAlert(type: .deleteDiary, title: "정말 일기를 삭제할까요?", message: "아직 답장이 오지 않았거나 삭제하고\n다시 작성한 일기는 답장을 받을 수 없어요.", rightButtonText: "삭제")
+
                 })
             })
             .disposed(by: disposeBag)
