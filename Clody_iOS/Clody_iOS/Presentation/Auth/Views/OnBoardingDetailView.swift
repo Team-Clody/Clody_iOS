29814c06cd3c673e262a991bca0637c177b7ac74
Nya128 <nanalovesbap@gmail.com>
2024-07-19 08:32:00 +0900

    [Fix/#56] 온보딩 디테일뷰 서브타이틀 중앙정렬

--- Clody_iOS/Clody_iOS/Presentation/Auth/Views/OnBoardingDetailView.swift
+++ Clody_iOS/Clody_iOS/Presentation/Auth/Views/OnBoardingDetailView.swift
@@ -76,7 +76,11 @@ final class OnBoardingDetailView: BaseView {
             lineHeightMultiple: 1.5,
             align: .center
         )
-        subTitleLabel.attributedText = UIFont.pretendardString(text: type.subTitle, style: .body1_medium)
+        subTitleLabel.attributedText = UIFont.pretendardString(
+            text: type.subTitle,
+            style: .body1_medium,
+            align: .center
+        )
         imageView.image = type.image
     }
     
