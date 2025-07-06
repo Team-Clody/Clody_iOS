59c40a5a328f9c2e1990926e4c08c2142b6cbd57
kim-seonwoo <sw204011@gmail.com>
2025-07-06 19:09:56 +0900

    [Feat/#130] 로컬라이제이션 매니저 생성

---
+++ Clody_iOS/Clody_iOS/Global/Manager/LocalizationManager.swift
@@ -0,0 +1,29 @@
+//
+//  LocalizationManager.swift
+//  Clody_iOS
+//
+//  Created by Seonwoo Kim on 7/6/25.
+//
+
+import Foundation
+
+enum AppLanguage: String {
+    case korean = "ko"
+    case english = "en"
+}
+
+final class LanguageManager {
+    static let shared = LanguageManager()
+    
+    private init() {
+        let langCode = Locale.current.languageCode
+        self.currentLanguage = (langCode == "ko") ? .english : .english
+        print(currentLanguage, "🎁")
+    }
+
+    private let currentLanguage: AppLanguage
+
+    var isKorean: Bool {
+        currentLanguage == .korean
+    }
+}
