c8da1f7067d2158fb7a8ea3d40aa87ef7443cf63
kim-seonwoo <sw204011@gmail.com>
2025-07-06 19:10:10 +0900

    [Feat/#130] 영어 스플래시 이미지 도입

--- Clody_iOS/Clody_iOS/Presentation/Auth/ViewControllers/SplashViewController.swift
+++ Clody_iOS/Clody_iOS/Presentation/Auth/ViewControllers/SplashViewController.swift
@@ -33,7 +33,7 @@ final class SplashViewController: UIViewController {
         }
         
         introImageView.do {
-            $0.image = .imgSlogan
+            $0.image = LanguageManager.shared.isKorean ? .imgSlogan : .imgSloganEn
             $0.contentMode = .scaleAspectFit
         }
         
