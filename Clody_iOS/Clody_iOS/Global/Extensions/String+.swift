03926f9a99fb6fd22ded8632397c87b0e4b19b1b
김나연 <nanalovesbap@gmail.com>
2025-07-17 18:22:31 +0900

    [Feat/#143] 이용약관, 개인정보 처리방침, 공지사항, 제안/문의하기 URL 현지화

--- Clody_iOS/Clody_iOS/Global/Extensions/String+.swift
+++ Clody_iOS/Clody_iOS/Global/Extensions/String+.swift
@@ -23,10 +23,10 @@ extension String {
     }
     
     enum TermsURL {
-        static let terms = "https://www.notion.so/1c7e3fedb3f4802c8db1f3056c03973f?pvs=21"
-        static let privacy = "https://www.notion.so/1c7e3fedb3f48024a334c8116255b378?pvs=21"
-        static let announcement = "https://www.notion.so/1c7e3fedb3f48029b36cf9d76c5fb6d6?pvs=21"
-        static let contactUs = "https://docs.google.com/forms/d/e/1FAIpQLSeCS3Z9ctFyqHZH7qkryOEQYQdhvNCMPT6QJ3J2GQw86WId4Q/viewform"
+        static let terms = String(localized: "termsURL.terms", comment: "서비스 이용약관")
+        static let privacy = String(localized: "termsURL.privacy", comment: "개인정보 처리방침")
+        static let announcement = String(localized: "termsURL.announcement", comment: "공지사항")
+        static let contactUs = String(localized: "termsURL.contactUs", comment: "제안/문의하기")
     }
     
     enum List {
