9c042d90f54651e14d396b9b386ca64d49084d5f
kim-seonwoo <sw204011@gmail.com>
2025-07-24 08:24:29 +0700

    [Fix/#151] API Constant 변경

--- Clody_iOS/Clody_iOS/Network/Environment/APIConstant.swift
+++ Clody_iOS/Clody_iOS/Network/Environment/APIConstant.swift
@@ -41,7 +41,7 @@ extension APIConstants{
         [contentType: applicationJSON,
                 auth: Bearer + UserManager.shared.accessTokenValue,
             timeZone: LocalizationConstant.timeZoneCode,
-      acceptLanguage: LocalizationConstant.languageCode
+      acceptLanguage: LocalizationConstant.Calendar.calendarLocale.identifier
         ]
     }
 }
